45d4e1c30ec296a1400d627cb98f09b44abd0150
Jeremy Kun <j2kun@users.noreply.github.com>
2019-06-12 22:23:56 -0700

    continue working

--- code/silent_duel.py
+++ code/silent_duel.py
@@ -1,7 +1,11 @@
 from collections import deque
 from dataclasses import dataclass
+from typing import Any
+from typing import List
 from typing import NewType
+import random
 
+from sympy import Expr
 from sympy import Integral
 from sympy import Lambda
 from sympy import N
@@ -12,6 +16,27 @@ from sympy.solvers import solve
 
 
 SuccessFn = NewType('SuccessFn', Lambda)
+
+
+def solve_unique_real(expr, var, solution_min=0, solution_max=1):
+    '''
+    Solve an equation and return any solution in the given range. This should
+    be used when the caller knows that there is a unique solution to the
+    equation in the given range.
+
+    If the range bounds are set to None, then the assumption is that there is a
+    unique global real solution. By default the range bound is [0,1].
+    '''
+    # print("solving {} = 0 for {}".format(expr, var))
+    solutions = solve(expr, var)
+    solutions = [x for x in solutions if x.is_real]
+    if solution_min is not None:
+        solutions = [x for x in solutions if x >= solution_min]
+    if solution_max is not None:
+        solutions = [x for x in solutions if x <= solution_max]
+
+    assert len(solutions) == 1, "Expected unique solution but found {}".format(solutions)
+    return solutions[0]
 
 
 @dataclass
@@ -27,11 +52,112 @@ class SilentDuelInput:
     player_2_action_success: SuccessFn
 
 
-def f_star(
-        player_action_success,
-        opponent_action_success,
-        variable,
-        larger_transition_times):
+@dataclass
+class ActionDistribution:
+    '''The interval on which this distribution occurs.'''
+    support_start: float
+    support_end: float
+
+    '''The cumulative density function for the distribution.'''
+    cumulative_density_function: Expr
+
+    def sample(self):
+        uniform_random_number = random.random()
+        x = Symbol('x', positive=True)
+        return solve_unique_real(
+            self.cumulative_density_function(x) - uniform_random_number,
+            x,
+            solution_min=self.support_start,
+            solution_max=self.support_end,
+        )
+
+
+@dataclass
+class SilentDuelOutput:
+    '''
+    The distribution functions representing the probability of taking each
+    action.
+    '''
+    action_distributions: List[ActionDistribution]
+
+    def sample_game_strategy(self):
+        return [d.sample() for d in self.action_distributions]
+
+    '''
+    Returns the ordered list of action transition times for this player.
+    '''
+
+    def transition_times(self):
+        times = [self.action_distributions[0].support_start]
+        times.extend([d.support_end for d in self.action_distributions])
+        return times
+
+
+@dataclass
+class IntermediateState:
+    '''Class representing the intermediate state of the silent
+    duel construction algorithm. This class is mutated as the
+    algorithm progresses.'''
+
+    '''
+    A list of the transition times compute thus far. This field
+    maintains the invariant of being sorted. Thus, the first element
+    in the list is a_{i + 1}, the most recently computed value of
+    player 1's transition times, and the last element is a_{n + 1} = 1.
+    This value is set on initializtion with `new`.
+    '''
+    player_1_transition_times: List[Expr]
+
+    '''
+    Same as player_1_transition_times, but for player 2 with b_j
+    and b_m.
+    '''
+    player_2_transition_times: List[Expr]
+
+    '''
+    The values of h_i, normalizing constants for the action
+    probability distributions for player 1. Has the same sorting
+    invariant as the transition time lists.
+    '''
+    player_1_normalizing_constants: List[Expr]
+
+    '''
+    Same as player_1_normalizing_constants, but for player 2,
+    i.e., the k_j normalizing constants.
+    '''
+    player_2_normalizing_constants: List[Expr]
+
+    @staticmethod
+    def new():
+        '''
+        Create a new state object, and set a_{n+1} = 1, b_{m+1}=1.
+        '''
+        return IntermediateState(
+            player_1_transition_times=deque([1]),
+            player_2_transition_times=deque([1]),
+            player_1_normalizing_constants=deque([]),
+            player_2_normalizing_constants=deque([]),
+        )
+
+    def add_p1(self, transition_time, normalizing_constant):
+        self.player_1_transition_times.appendleft(transition_time)
+        self.player_1_normalizing_constants.appendleft(normalizing_constant)
+
+    def add_p2(self, transition_time, normalizing_constant):
+        self.player_2_transition_times.appendleft(transition_time)
+        self.player_2_normalizing_constants.appendleft(normalizing_constant)
+
+
+"""
+The rest  of this file contains functions constructing the optimal strategy
+for each player.
+"""
+
+
+def f_star(player_action_success: SuccessFn,
+           opponent_action_success: SuccessFn,
+           variable: Symbol,
+           larger_transition_times: List[float]) -> Expr:
     '''Compute f^* as in Restrepo '57.
 
     The inputs can be chosen so that the appropriate f^* is built
@@ -53,111 +179,133 @@ def f_star(
     )
 
 
-def compute_a_n_and_b_m(silent_duel_input, alpha=0, beta=0):
-    P = silent_duel_input.player_1_action_success
-    Q = silent_duel_input.player_2_action_success
+def compute_ai_and_bj(duel_input: SilentDuelInput,
+                      intermediate_state: IntermediateState,
+                      alpha: float = 0,
+                      beta: float = 0):
+    '''
+    Compute a pair of a_i and b_j transition times for both players,
+    using the intermediate state of the algorithm computed so far.
+
+    This function also computes a_n and b_m when the intermediate_state
+    input has no larger transition times for the opposite player. In
+    those cases, the integrals and equations being solved are slightly
+    different; they include some terms involving alpha and beta. In all
+    other cases, the alpha and beta parameters are unused.
+    '''
+    P = duel_input.player_1_action_success
+    Q = duel_input.player_2_action_success
     t = Symbol('t0', positive=True)
-    a_n = Symbol('a_n', positive=True)
-    b_m = Symbol('b_m', positive=True)
+    a_i = Symbol('a_i', positive=True)
+    b_j = Symbol('b_j', positive=True)
 
-    p1_fstar = f_star(P, Q, t, [])
-    p2_fstar = f_star(Q, P, t, [])
+    p1_transitions = intermediate_state.player_1_transition_times
+    p2_transitions = intermediate_state.player_2_transition_times
 
-    # the a_n part
-    a_n_integral = Integral(
-        ((1 + alpha) - (1 - alpha) * P(t)) * p1_fstar, (t, a_n, 1))
-    a_n_integrated = a_n_integral.doit()
-    P_a_n_solutions = solve(a_n_integrated - 2 * (1 - alpha), P(a_n))
-    P_a_n_solutions = [x for x in P_a_n_solutions if x.is_real]
-    P_a_n = Max(*P_a_n_solutions)
-    print("P(a_n) = %s" % P_a_n)
+    # the left end of the transitions arrays contain the smallest
+    # (latest computed) transition time for each player.
+    # these are set to 1 for an empty intermediate state, i.e. for a_n, b_m
+    a_i_plus_one = p1_transitions[0]
+    b_j_plus_one = p2_transitions[0]
+    computing_a_n = a_i_plus_one == 1
+    computing_b_m = b_j_plus_one == 1
 
-    a_n_solutions = solve(P(a_n) - P_a_n, a_n)
-    a_n_solutions_in_range = [soln for soln in a_n_solutions if 0 < soln <= 1]
-    assert len(a_n_solutions_in_range) == 1
-    a_n = a_n_solutions_in_range[0]
-    print("a_n = %s" % a_n)
+    # the a_i part
+    if computing_a_n:
+        p1_fstar = f_star(P, Q, t, [])
+        p1_integrand = ((1 + alpha) - (1 - alpha) * P(t)) * p1_fstar
+        P_a_i_target = 2 * (1 - alpha)
+    else:
+        p1_fstar_parameters = list(p2_transitions)[:-1]  # ignore b_{m+1} = 1
+        p1_fstar = f_star(P, Q, t, p1_fstar_parameters)
+        p1_integrand = (1 - P(t)) * p1_fstar
+        P_a_i_target = 1 / intermediate_state.player_1_normalizing_constants[0]
 
-    # the b_m part
-    b_m_integral = Integral(
-        ((1 + beta) - (1 - beta) * Q(t)) * p2_fstar, (t, b_m, 1))
-    b_m_integrated = b_m_integral.doit()
-    Q_b_m_solutions = solve(b_m_integrated - 2 * (1 - beta), Q(b_m))
-    Q_b_m_solutions = [x for x in Q_b_m_solutions if x.is_real]
-    Q_b_m = Max(*Q_b_m_solutions)
-    print("Q(b_m) = %s" % Q_b_m)
+    a_i_integral = Integral(p1_integrand, (t, a_i, a_i_plus_one))
+    a_i_integrated = a_i_integral.doit()
+    P_a_i = solve_unique_real(a_i_integrated - P_a_i_target, P(a_i))
+    print("P(a_i) = %s" % P_a_i)
+    a_i = solve_unique_real(P(a_i) - P_a_i, a_i, solution_min=0, solution_max=a_i_plus_one)
+    print("a_i = %s" % a_i)
 
-    b_m_solutions = solve(Q(b_m) - Q_b_m, b_m)
-    b_m_solutions_in_range = [soln for soln in b_m_solutions if 0 < soln <= 1]
-    assert len(b_m_solutions_in_range) == 1
-    b_m = b_m_solutions_in_range[0]
-    print("b_m = %s" % b_m)
+    # the b_j part
+    if computing_b_m:
+        p2_fstar = f_star(Q, P, t, [])
+        p2_integrand = ((1 + beta) - (1 - beta) * P(t)) * p2_fstar
+        Q_b_j_target = 2 * (1 - beta)
+    else:
+        p2_fstar_parameters = list(p1_transitions)[:-1]  # ignore a_{n+1} = 1
+        p2_fstar = f_star(Q, P, t, p2_fstar_parameters)
+        p2_integrand = (1 - P(t)) * p2_fstar
+        Q_b_j_target = 1 / intermediate_state.player_2_normalizing_constants[0]
 
-    # the h part
-    h_n_integral = Integral(p1_fstar, (t, a_n, 1))
-    h_n_integrated = h_n_integral.doit()
-    h_n = (1 - alpha) / h_n_integrated
-    print("h_n = %s" % h_n)
+    b_j_integral = Integral(p2_integrand, (t, b_j, b_j_plus_one))
+    b_j_integrated = b_j_integral.doit()
+    Q_b_j = solve_unique_real(b_j_integrated - Q_b_j_target, Q(b_j))
+    # print("Q(b_j) = %s" % Q_b_j)
+    b_j = solve_unique_real(Q(b_j) - Q_b_j, b_j, solution_min=0, solution_max=b_j_plus_one)
+    # print("b_j = %s" % b_j)
 
-    # the k part
-    k_m_integral = Integral(p2_fstar, (t, b_m, 1))
-    k_m_integrated = k_m_integral.doit()
-    k_m = (1 - beta) / k_m_integrated
-    print("k_m = %s" % k_m)
+    # the h_i part
+    h_i_integral = Integral(p1_fstar, (t, a_i, a_i_plus_one))
+    h_i_integrated = h_i_integral.doit()
+    h_i_numerator = (1 - alpha) if computing_a_n else 1
+    h_i = h_i_numerator / h_i_integrated
+    # print("h_i = %s" % h_i)
 
-    return (a_n, b_m, h_n, k_m)
+    # the k_j part
+    k_j_integral = Integral(p2_fstar, (t, b_j, b_j_plus_one))
+    k_j_integrated = k_j_integral.doit()
+    k_j_numerator = (1 - beta) if computing_b_m else 1
+    k_j = k_j_numerator / k_j_integrated
+    # print("k_j = %s" % k_j)
+
+    return (a_i, b_j, h_i, k_j)
 
 
-def compute_as_and_bs(duel_input, alpha=0, beta=0):
-    '''Compute the a's and b's for the silent duel.'''
-    P = prob_fun
+def compute_as_and_bs(duel_input: SilentDuelInput,
+                      alpha: float = 0,
+                      beta: float = 0) -> IntermediateState:
+    '''
+    Compute the a's and b's for the silent duel, given a fixed
+    alpha and beta as input.
+    '''
     t = Symbol('t0', positive=True)
 
-    a_n, h_n = compute_a_n(prob_fun, alpha=alpha)
+    p1_index = duel_input.player_1_action_count
+    p2_index = duel_input.player_2_action_count
+    intermediate_state = IntermediateState.new()
 
-    normalizing_constants = deque([h_n])
-    transitions = deque([a_n])
+    while p1_index > 0 and p2_index > 0:
+        # compute a new a_i and b_j, keeping the larger as the next parameter
+        (a_i, b_j, h_i, k_j) = compute_ai_and_bj(duel_input, intermediate_state)
 
-    for step in range(n):
-        # prepending new a's and h's to the front of the list
-        last_a = transitions[0]
-        last_h = normalizing_constants[0]
-        next_a = Symbol('a', positive=True)
+        # the larger of a_n, b_m is kept as a parameter, then the other will be repeated
+        # in the next iteration; e.g., a_{n-1} and b_m (the latter using a_n in its f*)
 
-        next_a_integral = Integral(
-            (1 - P(t)) * f_star(P, t, transitions), (t, next_a, last_a))
-        next_a_integrated = next_a_integral.doit()
-        # print("%s" % next_a_integrated)
-        P_next_a_solutions = solve(next_a_integrated - 1 / last_h, P(next_a))
-        print("P(a_{n-%d}) is one of %s" % (step + 1, P_next_a_solutions))
-        P_next_a = Max(*P_next_a_solutions)
+                                # maybe check for equality first and do both???
+        if a_i > b_j:
+            intermediate_state.add_p1(a_i, h_i)
+            print("a_{} = {}, h_{} = {}".format(p1_index, a_i, p1_index, h_i))
+            p1_index -= 1
+        else:
+            intermediate_state.add_p2(b_j, k_j)
+            print("b_{} = {}, k_{} = {}".format(p2_index, b_j, p2_index, k_j))
+            p2_index -= 1
 
-        next_a_solutions = solve(P(next_a) - P_next_a, next_a)
-        next_a_solutions_in_range = [
-            soln for soln in next_a_solutions if 0 < soln <= 1]
-        assert len(next_a_solutions_in_range) == 1
-        next_a_soln = next_a_solutions_in_range[0]
-        print("a_{n-%d} = %s" % (step + 1, next_a_soln))
-
-        next_h_integral = Integral(
-            f_star(P, t, transitions), (t, next_a_soln, last_a))
-        next_h = 1 / next_h_integral.doit()
-        print("h_{n-%d} = %s" % (step + 1, next_h))
-
-        transitions.appendleft(next_a_soln)
-        normalizing_constants.appendleft(next_h)
-
-    return transitions
+    return intermediate_state
 
 
 x = Symbol('x')
 P = Lambda((x,), x)
 Q = Lambda((x,), x**2)
+
 duel_input = SilentDuelInput(
     player_1_action_count=3,
     player_2_action_count=3,
     player_1_action_success=P,
     player_2_action_success=P,
 )
-compute_a_n_and_b_m(duel_input)
-# compute_as_and_bs(Lambda((x,), x), 10)
+compute_as_and_bs(duel_input, alpha=0, beta=0)
+
+f = ActionDistribution(support_start=0, support_end=1, cumulative_density_function=Q)

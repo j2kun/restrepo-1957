bced7d69ffa3f6e91907ba2096c7d013591c35c0
Jeremy Kun <j2kun@users.noreply.github.com>
2019-07-17 12:28:01 -0700

    add examples in standalone runnables

---
+++ code/example2.py
@@ -0,0 +1,21 @@
+from silent_duel import *
+
+from sympy import Lambda
+from sympy import Symbol
+
+
+x = Symbol('x')
+P = Lambda((x,), x)
+Q = Lambda((x,), x**2)
+
+duel_input = SilentDuelInput(
+    player_1_action_count=1,
+    player_2_action_count=1,
+    player_1_action_success=P,
+    player_2_action_success=Q,
+)
+
+print("Input: {}".format(duel_input))
+output = optimal_strategies(duel_input)
+print(output)
+output.validate()
--- code/example3.py
+++ code/example3.py
@@ -1,8 +1,6 @@
 from silent_duel import *
 
-from sympy import Expr
 from sympy import Lambda
-from sympy import N
 from sympy import Symbol
 
 
@@ -12,10 +10,12 @@ Q = Lambda((x,), x**2)
 
 duel_input = SilentDuelInput(
     player_1_action_count=3,
-    player_2_action_count=4,
+    player_2_action_count=3,
     player_1_action_success=P,
     player_2_action_success=Q,
 )
 
+print("Input: {}".format(duel_input))
 output = optimal_strategies(duel_input)
-output.validate(err_on_fail=False)
+print(output)
+output.validate()
